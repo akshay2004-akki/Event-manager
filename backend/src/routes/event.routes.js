bc48d3d436545166d8c3789a94851f414c1f710e
Akshay anand <akshay1622004@gmail.com>
2025-04-05 17:39:00 +0530

    correction in event routes

--- backend/src/routes/event.routes.js
+++ backend/src/routes/event.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { createEvent , getEvent } from "../controllers/event.controller";
+import { createEvent , getEvent } from "../controllers/event.controller.js";
 const router = Router();
 
 router.post('/createEvent', createEvent);
