561669e36b2645495521654b0c45ce3849ac8b90
Akshay anand <akshay1622004@gmail.com>
2025-04-08 23:08:45 +0530

    login update

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -23,7 +23,7 @@ const Login = ({loggedIn, setLoggedIn}) => {
       console.log(response);
       
 
-      if(response.statusText=="OK"){
+      if(response.status==200){
         setLoggedIn(true)
       }
       
